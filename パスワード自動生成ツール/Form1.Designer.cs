1cab432060578f61e7565bff38964ec606061f5e
YoshinoriKamimoto <k.yoshinori0210@gmail.com>
2023-03-17 18:27:32 +0900

    パスワード自動生成ツール

---
+++ パスワード自動生成ツール/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace パスワード自動生成ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GeneratePasswordButton = new System.Windows.Forms.Button();
+            this.WordCountTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NewPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GeneratePasswordButton
+            // 
+            this.GeneratePasswordButton.Location = new System.Drawing.Point(147, 135);
+            this.GeneratePasswordButton.Name = "GeneratePasswordButton";
+            this.GeneratePasswordButton.Size = new System.Drawing.Size(85, 30);
+            this.GeneratePasswordButton.TabIndex = 0;
+            this.GeneratePasswordButton.Text = "パスワード生成";
+            this.GeneratePasswordButton.UseVisualStyleBackColor = true;
+            this.GeneratePasswordButton.Click += new System.EventHandler(this.GeneratePasswordButton_Click);
+            // 
+            // WordCountTextBox
+            // 
+            this.WordCountTextBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.WordCountTextBox.Location = new System.Drawing.Point(165, 96);
+            this.WordCountTextBox.MaxLength = 2;
+            this.WordCountTextBox.Name = "WordCountTextBox";
+            this.WordCountTextBox.Size = new System.Drawing.Size(46, 23);
+            this.WordCountTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(164, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文字数";
+            // 
+            // NewPasswordTextBox
+            // 
+            this.NewPasswordTextBox.Location = new System.Drawing.Point(64, 216);
+            this.NewPasswordTextBox.MaxLength = 32;
+            this.NewPasswordTextBox.Name = "NewPasswordTextBox";
+            this.NewPasswordTextBox.ReadOnly = true;
+            this.NewPasswordTextBox.Size = new System.Drawing.Size(259, 19);
+            this.NewPasswordTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(29, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(346, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "作成したいパスワードの文字数を入力して、パスワード生成ボタンをクリック";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(61, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 14);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "パスワード";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(219, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 14);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "※4－32文字";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 293);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GeneratePasswordButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WordCountTextBox);
+            this.Controls.Add(this.NewPasswordTextBox);
+            this.Name = "Form1";
+            this.Text = "パスワード自動生成ツール";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GeneratePasswordButton;
+        private System.Windows.Forms.TextBox WordCountTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NewPasswordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ パスワード自動生成ツール/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace パスワード自動生成ツール
+{
+    public partial class Form1 : Form
+    {
+        // コンストラクタ
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // パスワード生成ボタンClickイベント
+        private void GeneratePasswordButton_Click(object sender, EventArgs e)
+        {
+            NewPasswordTextBox.Clear();
+
+            // 生成するパスワードの文字数をテキストボックスから取得
+            string wordCntStr = WordCountTextBox.Text;
+
+            // 値を整数変換
+            int wordCnt;
+            if (int.TryParse(wordCntStr, out wordCnt) == false)
+            {
+                MessageBox.Show("文字数は数字で入力してください。",
+                                "入力値不正",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Exclamation);
+                WordCountTextBox.Focus();
+                WordCountTextBox.SelectAll();
+                return;
+            }
+
+            // 数値をチェック
+            if (wordCnt < 4 || wordCnt > 32)
+            {
+                MessageBox.Show("文字数は4～32文字で入力してください。",
+                                "入力値不正",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Exclamation);
+                WordCountTextBox.Focus();
+                WordCountTextBox.SelectAll();
+                return;
+            }
+
+            // パスワードを生成
+            StringBuilder sb = GeneratePassword(wordCnt);
+
+
+            // 画面に生成したパスワードを表示
+            NewPasswordTextBox.Text = sb.ToString();
+            MessageBox.Show("パスワードを生成しました。",
+                            "完了",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+            WordCountTextBox.Clear();
+
+        }
+
+
+        // パスワード生成メソッド
+        private StringBuilder GeneratePassword(int wordCnt)
+        {
+            // 生成したパスワードを保持する
+            StringBuilder sb = new StringBuilder();
+
+            // 数字、大文字英字、小文字英字、記号の最低文字数を設定
+            int numberCnt = 1;
+            int upperAlphabetCnt = 1;
+            int lowrAlphabetCnt = 1;
+            int symbolAlphabetCnt = 1;
+
+            // 入力された文字数から余剰文字数を計算
+            int extraCnt = wordCnt - (numberCnt + upperAlphabetCnt + lowrAlphabetCnt + symbolAlphabetCnt);
+
+            // 余剰文字数を、乱数を使用して各要素にランダムに配分
+            if (extraCnt > 0)
+            {
+                for (int i = 0; i < extraCnt; i++)
+                {
+                    int n = GetRandomNumbers(4);
+                    if (n == 0)
+                    {
+                        numberCnt++;
+                    }
+                    else if (n == 1)
+                    {
+
+                        upperAlphabetCnt++;
+                    }
+                    else if (n == 2)
+                    {
+                        lowrAlphabetCnt++;
+                    }
+                    else if (n == 3)
+                    {
+                        symbolAlphabetCnt++;
+                    }
+                }
+            }
+
+            // 設定された文字数分、各要素の値をランダムに取得
+            // 数字を取得
+            for (int i = 0; i < numberCnt; i++)
+            {
+                int n = GetRandomNumbers(10);
+
+                // 値を追加
+                sb.Append(n.ToString());
+            }
+
+            // 大文字英字を取得
+            for (int i = 0; i < upperAlphabetCnt; i++)
+            {
+                string word = GetRandomUppperAlphabet();
+
+                // 値を追加
+                sb.Append(word);
+            }
+
+            // 小文字英字を取得
+            for (int i = 0; i < lowrAlphabetCnt; i++)
+            {
+                string word = GetRandomLowerAlphabet();
+
+                // 値を追加
+                sb.Append(word);
+            }
+
+            // 記号を取得
+            for (int i = 0; i < symbolAlphabetCnt; i++)
+            {
+                string word = GetRandomSymbol();
+
+                // 値を追加
+                sb.Append(word);
+            }
+
+
+            // 生成したパスワードを返却
+            return sb;
+        }
+
+
+        // ランダムな数字を取得するメソッド
+        private int GetRandomNumbers(int maxValue)
+        {
+            // ランダムオブジェクトをインスタンス化
+            Random random = new Random();
+
+            // 0~指定された上限値までのランダムな数字を取得
+            int num = random.Next(maxValue);
+
+            return num;
+        }
+
+
+        // ランダムな大文字アルファベットを取得するメソッド
+        private string GetRandomUppperAlphabet()
+        {
+            // 使用する大文字アルファベットを定義
+            string[] array = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+            // 配列にアクセスするための添え字を乱数で取得
+            int index = GetRandomNumbers(array.Length);
+
+            // 配列から値を取得
+            string alphabet = array[index];
+
+            return alphabet;
+        }
+
+
+        // ランダムな小文字アルファベットを取得するメソッド
+        private string GetRandomLowerAlphabet()
+        {
+            // 使用する小文字アルファベットを定義
+            string[] array = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            // 配列にアクセスするための添え字を乱数で取得
+            int index = GetRandomNumbers(array.Length);
+
+            // 配列から値を取得
+            string alphabet = array[index];
+
+            return alphabet;
+        }
+
+
+        // ランダムな記号を取得するメソッド
+        private string GetRandomSymbol()
+        {
+            // 使用する記号を定義
+            string[] array = { "_", "!", "&", "%", "?", "$" };
+
+            // 配列にアクセスするための添え字を乱数で取得
+            int index = GetRandomNumbers(array.Length);
+
+            // 配列から値を取得
+            string symbol = array[index];
+
+            return symbol;
+        }
+    }
+}
